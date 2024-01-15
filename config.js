ce68845fab94d8ad60a32f8b8c8c8c8013a96c0c
harshsharma19991215 <hs19991215@gmail.com>
2024-01-15 15:18:23 +0530

    Update config.js, controllers, models, routes, and index.js

--- config.js
+++ config.js
@@ -1,11 +1,20 @@
 export const PORT = 3000;
+<<<<<<< HEAD
 export const MAIN_DB_URL = "mongodb+srv://harshtestserver:YFfvHSzmwKrj0YEL@cluster0.16l5ggx.mongodb.net/?retryWrites=true&w=majority";
 export const LOCAL_DB_URL = "mongodb://localhost:27017"
 export const DB_KEY = "/social-app"
+=======
+export const MAIN_DB_URL = "mongodb+srv://harshtestserver:YFfvHSzmwKrj0YEL@cluster0.16l5ggx.mongodb.net/?retryWrites=true&w=majority"
+export const DB_KEY = "social-app"
+>>>>>>> beb6883 ( Update config.js, controllers, models, routes, and index.js)
 export const JWT_SECRET_KEY = "fucku"
 export const PRODUCT_ID_PREFIX= "HARSH"
 
 // YFfvHSzmwKrj0YEL
 // harshtestserver
 
+<<<<<<< HEAD
 // mongodb+srv://harshtestserver:<password>@cluster0.16l5ggx.mongodb.net/?retryWrites=true&w=majority
+=======
+// mongodb+srv://harshtestserver:<password>@cluster0.16l5ggx.mongodb.net/?retryWrites=true&w=majority
+>>>>>>> beb6883 ( Update config.js, controllers, models, routes, and index.js)
